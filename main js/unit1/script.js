9107761c72bb8afadc4aa636fc467b6c4ba2c8ef
Olzhas <willdizzel02@gmail.com>
2021-03-16 22:07:50 +0600

    First commit

---
+++ main js/unit1/script.js
@@ -0,0 +1,65 @@
+console.log('--------------------');
+console.log(leftBorderWith);
+var leftBorderWith = 1;
+let second = 2;
+const pi = 3.14;
+console.log(leftBorderWith);
+console.log(second);
+console.log(pi);
+
+
+console.log('sdgf' * 3);
+console.log(34 / 0);
+//////////////////////////////////////////////////////////
+console.log('--------------------');
+let something;
+console.log(something);
+// console.log(itsNull)
+
+let person = {
+    name: 'Olzhas',
+    age: 22,
+    sex: 'male'
+};
+person.name = 'John';
+console.log(person);
+console.log(person.name);
+console.log(person["age"]);
+
+let arr = ['plum.png', 'orange.jpeg', 'apple.bmp'];
+console.log(arr[1]);
+
+//////////////////////////////////////////////////////////
+console.log('--------------------');
+// let answer = confirm('Are you okey?');
+// console.log(answer);
+// alert('hello world');
+// let x;
+// while(null==x){
+//     x = prompt("Are you here?","yes");
+// }
+
+///////////////////////////////////////////////////////////
+console.log('--------------------');
+console.log("arr " + " - object");
+console.log(4 + " - object");
+console.log(4 + +" - object");
+console.log(4 + +"5");
+
+console.log('--------------------');
+
+// let a = +prompt('Enter number: ', '0');
+// console.log(a);
+
+let incr = 10,
+    decr = 10;
+
+console.log(++incr);
+console.log(incr++);
+console.log(--decr);
+console.log(decr--);
+
+console.log(true&&true === ('2' == 2));
+
+let t = '123t';
+console.log(++t)
